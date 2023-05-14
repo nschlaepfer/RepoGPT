ca3a22535380797c488da87b1cf9e7c6c51d2c9c
nschlaepfer <nicolas.schlaepfer@ucdenver.edu>
2023-05-14 14:13:30 -0600

    GUI and error handling

--- src/code_processing.py
+++ src/code_processing.py
@@ -2,6 +2,7 @@ import openai
 import os
 from dotenv import load_dotenv
 import textwrap
+import libcst as cst
 
 
 load_dotenv()  # take environment variables from .env.
@@ -12,12 +13,41 @@ SERP_API_KEY = os.getenv("SERP_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX")
 
-load_dotenv()  # take environment variables from .env.
 
-OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 
 # Use the OpenAI API key from environment variables
 openai.api_key = OPEN_API_KEY
+
+
+import libcst as cst
+
+class FunctionAndClassVisitor(cst.CSTVisitor):
+    def __init__(self):
+        self.offsets = []
+
+    def visit_FunctionDef(self, node):
+        self.offsets.append(node.lines[0].start.line)
+
+    def visit_ClassDef(self, node):
+        self.offsets.append(node.lines[0].start.line)
+
+def split_code_into_chunks(code):
+    module = cst.parse_module(code)
+    visitor = FunctionAndClassVisitor()
+    module.visit(visitor)
+
+    lines = code.split('\n')
+    chunks = []
+    start = 0
+    for end in visitor.offsets:
+        chunks.append('\n'.join(lines[start:end]))
+        start = end
+    chunks.append('\n'.join(lines[start:]))
+    return chunks
+
+
+
+
 def comment_files(directory, gpt_version):
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -26,25 +56,32 @@ def comment_files(directory, gpt_version):
                 with open(file_path, "r") as f:
                     content = f.read()
 
-                # Split the content into chunks of approximately 2000 tokens each
-                # Note: This is not a precise split, as tokens in GPT-3 can vary in length
-                chunks = textwrap.wrap(content, 2000)
+                # Split the content into chunks based on function and class boundaries
+                chunks = split_code_into_chunks(content)
 
                 analysis = ""
+                commented_content = content  # Start with the original content
 
                 # Send each chunk to the OpenAI API for analysis
                 for chunk in chunks:
                     response = openai.ChatCompletion.create(
-                      model=f"gpt-{gpt_version}",  # Change the model according to the GPT version
-                      messages=[
-                        {"role": "system", "content": "You are a helpful assistant that analyzes and comments Python code. List any issues you find and suggest improvements and fixes. You can also suggest new features."},
-                        {"role": "user", "content": f"Analyze this code:\n{chunk}"}
-                      ],
-                      max_tokens=1000  # Adjust this as needed
+                        # Change the model according to the GPT version
+                        model=f"gpt-{gpt_version}",
+                        messages=[
+                            {"role": "system", "content": "You are a helpful assistant that analyzes and comments Python code. List any issues you find and suggest improvements and fixes. You can also suggest new features."},
+                            {"role": "user", "content": f"Analyze this code:\n{chunk}"}
+                        ],
+                        max_tokens=1000  # Adjust this as needed
                     )
 
-                    chunk_analysis = response['choices'][0]['message']['content'].strip()
+                    chunk_analysis = response['choices'][0]['message']['content'].strip(
+                    )
                     analysis += chunk_analysis + "\n"
+
+                    # Insert the analysis as a comment at the start of the chunk
+                    start_line = content[:content.find(chunk)].count('\n') + 1
+                    comment = "\n".join("# " + line for line in chunk_analysis.split("\n"))
+                    commented_content = commented_content[:start_line] + comment + commented_content[start_line:]
 
                 # Write the analysis to a new markdown file
                 new_file_path = file_path.replace(".py", "_repoGPT.md")
@@ -52,6 +89,14 @@ def comment_files(directory, gpt_version):
                     f.write(analysis)
 
                 print(f"Analysis written to {new_file_path}")
+
+                # Write the commented code to a new Python file
+                # This makes a new file. Can change maybe a setting to choose new file or overwrite old one.
+                new_file_path = file_path.replace(".py", "_commented.py")
+                with open(new_file_path, "w") as f:
+                    f.write(commented_content)
+
+                print(f"Commented code written to {new_file_path}")
 
 
 def create_readme(directory):
@@ -81,7 +126,8 @@ def create_readme(directory):
 
     final_readme_content = response['choices'][0]['message']['content'].strip()
 
-    readme_filename = "README.md" if "README.md" not in os.listdir(directory) else "readme_repoGPT.md"
+    readme_filename = "README.md" if "README.md" not in os.listdir(
+        directory) else "readme_repoGPT.md"
     readme_file_path = os.path.join(directory, readme_filename)
     with open(readme_file_path, "w") as f:
         f.write(final_readme_content)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,35 @@
+# src/main.py
+from flask import Flask, render_template, request
+from git_operations import clone_repo
 from code_processing import comment_files, create_readme
+from embedding_generation import generate_embeddings
+from pinecone_operations import upload_to_pinecone
 
-def main():
-    directory = "/Users/nico/RepoGPT/REPO"  # replace this with the directory of the repo you want to analyze
-    gpt_version = "3.5-turbo"  # replace with "4" if GPT-4 is available and you want to use it
+app = Flask(__name__)
 
-    # Test the comment_files function
-    comment_files(directory, gpt_version)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        repo_link = request.form.get('repo_link')
+        user_choice = request.form.get('user_choice')
 
-    # Test the create_readme function
-    create_readme(directory)
+        # Clone the repository
+        directory = clone_repo(repo_link)
+
+        if user_choice in ['1', '3']:
+            # Convert the code into embeddings and upload to Pinecone
+            embeddings = generate_embeddings(directory)
+            upload_to_pinecone(embeddings)
+
+        if user_choice in ['2', '3']:
+            # Comment all files and create README
+            comment_files(directory, "3.5-turbo")  # or "4" if GPT-4 is available
+            create_readme(directory)
+
+        return 'Operation completed!'
+    
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
